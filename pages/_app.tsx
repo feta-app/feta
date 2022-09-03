f3e98fb54c6d4464aa6bdc8c83db718c908e22e5
julianweng <julian.weng.us@gmail.com>
2022-09-03 10:53:38 -0400

    navbar template

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,14 @@ import '../styles/globals.css'
 import { ConvexProvider, ConvexReactClient } from 'convex/react'
 import clientConfig from "../convex/_generated/clientConfig";
 import { ChakraProvider } from '@chakra-ui/react';
+import Navbar from '../components/navbar';
 const convex = new ConvexReactClient(clientConfig);
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <ConvexProvider client={convex}>
+        <Navbar />
         <Component {...pageProps} />
       </ConvexProvider>
     </ChakraProvider>
