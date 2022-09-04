d61d7b686b623d448c66aed85212adf0de648e71
Anthony Li <me@anli.dev>
2022-09-04 00:46:32 -0400

    Add fancy login page
    it's so hacky but it's literlaly late at night no oen will know

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 
 import { ConvexProvider, ConvexReactClient } from 'convex/react'
 import clientConfig from "../convex/_generated/clientConfig";
-import { ChakraProvider } from '@chakra-ui/react';
+import { Button, ChakraProvider } from '@chakra-ui/react';
 import { ConvexProviderWithAuth0 } from 'convex/react-auth0';
 import Navbar from '../components/navbar';
 import convexConfig from '../convex.json';
@@ -10,6 +10,7 @@ import { createContext, Fragment, PropsWithChildren, useContext, useEffect, useS
 import storeUser from '../convex/storeUser';
 import { useMutation } from '../convex/_generated/react';
 import { LocationProvider, useLocationState } from '../components/location';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const convex = new ConvexReactClient(clientConfig);
 const authInfo = convexConfig.authInfo[0];
@@ -36,21 +37,46 @@ export function useUserID() {
   return useContext(UserIDContext);
 }
 
+function SuperHackyLoginPage() {
+  const { loginWithRedirect } = useAuth0();
+  
+  return <div className="super-hacky-page" style={{
+    height: "100vh",
+  }}>
+    <iframe src="https://get-feta.glitch.me" style={{
+      width: "100%",
+      height: "calc(100vh - 100px)",
+      border: "none",
+      margin: "0",
+    }} />
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    }}>
+      <Button size="lg" backgroundColor="black" colorScheme="blackAlpha" onClick={() => {
+        loginWithRedirect();
+      }}>Log in</Button>
+    </div>
+  </div>
+}
+
 function MyApp({ Component, pageProps }) {
   return (
-    <ConvexProviderWithAuth0
-      client={convex}
-      authInfo={authInfo}
-    >
-      <UserStorer>
-        <LocationProvider>
-          <ChakraProvider>
+    <ChakraProvider>
+      <ConvexProviderWithAuth0
+        client={convex}
+        authInfo={authInfo}
+        loggedOut={<SuperHackyLoginPage />}
+      >
+        <UserStorer>
+          <LocationProvider>
             <Navbar />
             <Component {...pageProps} />
-          </ChakraProvider>
-        </LocationProvider>
-      </UserStorer>
-    </ConvexProviderWithAuth0>
+          </LocationProvider>
+        </UserStorer>
+      </ConvexProviderWithAuth0>
+    </ChakraProvider>
   )
 }
 
